da8fea0d576107df8a5f99992fc3c338b55b81eb
QvvQV <131390208+QvvQV@users.noreply.github.com>
2024-08-28 10:40:10 +0300

    Update CardPage.java

--- src/test/java/ru/netology/page/CardPage.java
+++ src/test/java/ru/netology/page/CardPage.java
@@ -76,8 +76,8 @@ public class CardPage {
 
     public void fillValidField() { //заполнение полей верными данными
         inputCardNumber.setValue(DataHelper.getAproovedCard());
-        inputMonth.setValue(card.getMonth());
-        inputYears.setValue(card.getYear());
+        // inputMonth.setValue(card.getMonth());
+        // inputYears.setValue(card.getYear());
         inputOwner.setValue(DataHelper.generateValidCardHolder());
         inputCVV.setValue(DataHelper.generateCVC());
         clickContinueButton();
