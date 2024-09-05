f1bbda9dbef58c859fe088fe5098c322b3a4fcf5
QvvQV <131390208+QvvQV@users.noreply.github.com>
2024-09-05 13:28:27 +0300

    Update BuyGate.java

--- src/test/java/ru/netology/page/BuyGate.java
+++ src/test/java/ru/netology/page/BuyGate.java
@@ -46,28 +46,28 @@ public class BuyGate {
     }
 
     public void waitNotificationApproved() {
-        approvedOperation.shouldBe(visible, Duration.ofSeconds(15));
+        approvedOperation.shouldBe(visible); //Duration.ofSeconds(15)
         cancelField.click();
     }
 
     public void waitNotificationFailure() {
-        failureOperation.shouldBe(visible, Duration.ofSeconds(15));
+        failureOperation.shouldBe(visible); //Duration.ofSeconds(15)
     }
 
     public void waitNotificationWrongFormat() {
-        wrongFormatError.shouldBe(visible, Duration.ofSeconds(15));
+        wrongFormatError.shouldBe(visible); //Duration.ofSeconds(15)
     }
 
     public void waitNotificationExpirationDateError() {
-        cardExpirationDateError.shouldBe(visible, Duration.ofSeconds(15));
+        cardExpirationDateError.shouldBe(visible); //Duration.ofSeconds(15)
     }
 
     public void waitNotificationExpiredError() {
-        cardExpiredError.shouldBe(visible, Duration.ofSeconds(15));
+        cardExpiredError.shouldBe(visible); //Duration.ofSeconds(15)
     }
 
     public void waitNotificationWrongFormat4Fields() {
         wrongFormat4Error.shouldHave(CollectionCondition.size(4));
-        requiredFieldError.shouldBe(visible, Duration.ofSeconds(15));
+        requiredFieldError.shouldBe(visible); //Duration.ofSeconds(15)
     }
 }
